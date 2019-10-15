141b52ecbeadac6682fb13f35ba440c2c4d8d53e
pranav-d1993 <pranav.d1993@tamu.edu>
2019-10-15 11:52:56 -0500

    added basic camera movements

--- Pranav/virtua_robot_vision/Assets/Scripts/camera_movement.cs
+++ Pranav/virtua_robot_vision/Assets/Scripts/camera_movement.cs
@@ -1,18 +1,64 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class camera_movement : MonoBehaviour
+public class move_cam : MonoBehaviour
 {
+
+    public float x_rotation = 0.0f;
+    public float y_rotation = 0.0f;
+    public float z_rotation = 0.0f;
+    public float camera_speed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 rot = transform.localRotation.eulerAngles;
+        float xRot = rot.x;
+        float yRot = rot.y;
+        float zRot = rot.z;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * Time.deltaTime;
+        }
+
         
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            
+            x_rotation = transform.localRotation.eulerAngles.x + camera_speed * Time.deltaTime;
+            y_rotation = transform.localRotation.eulerAngles.y;// + Time.deltaTime;
+            z_rotation = transform.localRotation.eulerAngles.z;// + Time.deltaTime;
+            transform.rotation = Quaternion.Euler(x_rotation, y_rotation, z_rotation);
+        }
+        if (Input.GetKey(KeyCode.O))
+        {
+            x_rotation = transform.localRotation.eulerAngles.x - camera_speed * Time.deltaTime;
+            y_rotation = transform.localRotation.eulerAngles.y;// + Time.deltaTime;
+            z_rotation = transform.localRotation.eulerAngles.z;// + Time.deltaTime;
+            transform.rotation = Quaternion.Euler(x_rotation, y_rotation, z_rotation);
+        }
     }
 }
