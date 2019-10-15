929899ea7367805ae946eb70306053752773f01c
pranav-d1993 <pranav.d1993@tamu.edu>
2019-10-15 12:18:06 -0500

    removed bugs and fixed the camera movement code

--- Pranav/virtua_robot_vision/Assets/Scripts/camera_movement.cs
+++ Pranav/virtua_robot_vision/Assets/Scripts/camera_movement.cs
@@ -1,8 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move_cam : MonoBehaviour
+public class camera_movement : MonoBehaviour
 {
 
     public float x_rotation = 0.0f;
@@ -26,39 +26,46 @@ public class move_cam : MonoBehaviour
         Vector3 rot = transform.localRotation.eulerAngles;
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += Vector3.right * Time.deltaTime;
+            x_rotation = transform.localRotation.eulerAngles.x;// + camera_speed * Time.deltaTime;
+            y_rotation = transform.localRotation.eulerAngles.y + camera_speed * Time.deltaTime;// + Time.deltaTime;
+            z_rotation = transform.localRotation.eulerAngles.z;// + Time.deltaTime;
+            transform.rotation = Quaternion.Euler(x_rotation, y_rotation, z_rotation);
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += Vector3.down * Time.deltaTime;
+            transform.position -= transform.forward * Time.deltaTime*camera_speed;//+= Vector3.down * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += Vector3.up * Time.deltaTime;
+            transform.position += transform.forward * Time.deltaTime * camera_speed;//Vector3.up * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += Vector3.left * Time.deltaTime;
+            x_rotation = transform.localRotation.eulerAngles.x;// + camera_speed * Time.deltaTime;
+            y_rotation = transform.localRotation.eulerAngles.y - camera_speed * Time.deltaTime;// + Time.deltaTime;
+            z_rotation = transform.localRotation.eulerAngles.z;// + Time.deltaTime;
+            transform.rotation = Quaternion.Euler(x_rotation, y_rotation, z_rotation);
         }
 
-        
 
+        /*
         if (Input.GetKey(KeyCode.I))
         {
-            
-            x_rotation = transform.localRotation.eulerAngles.x + camera_speed * Time.deltaTime;
-            y_rotation = transform.localRotation.eulerAngles.y;// + Time.deltaTime;
+
+            x_rotation = transform.localRotation.eulerAngles.x;// + camera_speed * Time.deltaTime;
+            y_rotation = transform.localRotation.eulerAngles.y + camera_speed * Time.deltaTime;// + Time.deltaTime;
             z_rotation = transform.localRotation.eulerAngles.z;// + Time.deltaTime;
             transform.rotation = Quaternion.Euler(x_rotation, y_rotation, z_rotation);
         }
         if (Input.GetKey(KeyCode.O))
         {
-            x_rotation = transform.localRotation.eulerAngles.x - camera_speed * Time.deltaTime;
-            y_rotation = transform.localRotation.eulerAngles.y;// + Time.deltaTime;
+            x_rotation = transform.localRotation.eulerAngles.x ;
+            y_rotation = transform.localRotation.eulerAngles.y - camera_speed * Time.deltaTime;// + Time.deltaTime;
             z_rotation = transform.localRotation.eulerAngles.z;// + Time.deltaTime;
             transform.rotation = Quaternion.Euler(x_rotation, y_rotation, z_rotation);
         }
+        */
     }
 }
