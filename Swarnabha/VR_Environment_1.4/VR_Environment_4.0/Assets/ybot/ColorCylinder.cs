bab1a81f84342224baa84c9561d24cdf0fb57959
Swarnabha <swarnabha.roy@oyorooms.com>
2019-11-03 13:34:13 -0600

    YBOT folder added

---
+++ Swarnabha/VR_Environment_1.4/VR_Environment_4.0/Assets/ybot/ColorCylinder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorCylinder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+
+        var cylinderRenderer = cylinder.GetComponent<Renderer>();
+
+        //Call SetColor using the shader property name "_Color" and setting the color to red
+        cylinderRenderer.material.SetColor("_Color", Color.red);
+    }
+}
